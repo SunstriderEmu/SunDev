0a7cb9b9daaf8b96aaae6eb419579d6c22e89171
Nasty <allia.sinc@gmail.com>
2015-11-17 17:47:06 +0100

    Adapt SunWaypoint to the new Review system

--- app/controllers/Waypoints.php
+++ app/controllers/Waypoints.php
@@ -12,28 +12,11 @@ $app->get('/waypoints/path/{path}', function ($path) use ($app) {
 
 $app->get('/waypoints/entry/{entry}', function ($entry) use ($app) {
 	$creature 	= new SUN\Domain\Creature(["entry" => $entry]);
-	$manager	= new SUN\DAO\SmartAIDAO($app);
+	$manager	= new SUN\DAO\CreatureDAO($app);
 	$path		= new SUN\DAO\PathDAO($app);
 	$info = [
 		"name"	=> $manager->findCreatureEntryName($creature)->getName(),
 		"free"	=>	$path->getEntryPath($entry),
 	];
 	return json_encode($info);
-});
-
-$app->post('/waypoints/transfer', function () use ($app) {
-	$info		= json_decode($_POST['info']);
-	$manager	= new SUN\DAO\PathDAO($app);
-	$manager->sendTransfer($info);
-	$manager->setTransfer($info, 'test');
-	return "Success";
-});
-
-$app->post('/waypoints/pause', function () use ($app) {
-	$info		= json_decode($_POST['info']);
-	$manager	= new SUN\DAO\ReviewDAO($app);
-	$manager->createReview($info);
-	$manager	= new SUN\DAO\PathDAO($app);
-	$manager->setPause($info, 'test');
-	return "Success";
 });
--- src/DAO/PathDAO.php
+++ src/DAO/PathDAO.php
@@ -2,71 +2,15 @@
 
 namespace SUN\DAO;
 
-use PDO;
-
 class PathDAO extends DAO {
 
 	public function getPath($path) {
 		$point = $this->test->fetchAssoc('SELECT COUNT(point) as count FROM waypoint_data WHERE id = ?', array(intval($path)));
-
-		return $path = [
-			"path" => $path,
-			"points" => $point['count'],
-			];
+		return $path = [ "path" => $path, "points" => $point['count'] ];
 	}
 
 	public function getEntryPath($entry) {
-		$query  = $this->test->prepare('SELECT entry FROM waypoints WHERE entry = :entry');
-		$query->bindValue(':entry', $entry, PDO::PARAM_INT);
-		$query->execute();
-
+		$query = $this->test->executeQuery('SELECT entry FROM waypoints WHERE entry = ?', array($entry));
 		return $query->rowCount() > 0 ? false : true;
-	}
-
-	public function setTransfer($info, $db) {
-		$name  = $this->$db->fetchAssoc('SELECT name FROM creature_template WHERE entry = ?', array(intval($info->entry)));
-
-		// Delete previous waypoints
-		$this->$db->executeQuery('DELETE FROM waypoints WHERE entry = ?', array(intval($info->entry)));
-
-		// Points to transfer
-		$points = $this->$db->fetchAll('SELECT point, position_x, position_y, position_z FROM waypoint_data WHERE id = ?', array(intval($info->path)));
-
-		// Transfer
-		$insert = 'INSERT IGNORE INTO waypoints (entry, pointid, position_x, position_y, position_z, point_comment';
-		foreach($points as $point) {
-			$insert .= "({$info->entry}, {$point['point']}, {$point['position_x']}, {$point['position_y']}, {$point['position_z']}, {$name['name']}),'";
-		}
-		$insert = rtrim($insert, ',');
-
-		$this->$db->executeQuery($insert);
-	}
-
-	public function setPause($info, $db) {
-		$this->$db->executeQuery('UPDATE waypoint_data SET delay = ? WHERE id = ? AND point = ?;', array(intval($info->delay), intval($info->path), intval($info->point)));
-	}
-
-	public function sendTransfer($info) {
-		$get = $this->test->fetchAssoc('SELECT COUNT(*) as count FROM waypoints WHERE entry = ?', array(intval($info->entry)));
-		$review = [
-			"entry" 	=> $info->entry,
-			"source"	=> $info->source,
-			"user"		=> $info->user,
-			'info1' 	=> $info->path
-		];
-		if($get['count'] != 0)
-			$review['info1'] = -1 * intval($review['info1']);
-		$manager = new ReviewDAO($this->app);
-		$manager->createReview($review);
-	}
-
-	public function sendPause($info) {
-		return $this->tools->executeQuery('INSERT INTO smart_review (entryorguid, source_type, path, info1, user, date) VALUES (?, ?, ?, ?, ?, ?)
-											 ON DUPLICATE KEY UPDATE path = ?, info1 = ?, date = ?, user = ?', array(
-				"entry"	=> intval($info->path),
-				"source"	=> intval($info->source),
-			"point"	=> intval($info->point),
-			"delay"	=> intval($info->delay),
-			"user" => intval($info->user), time()));
 	}
 } 

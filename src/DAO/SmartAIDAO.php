519e749d86daab58509f72b0c38dce60511c7abf
Nasty <allia.sinc@gmail.com>
2016-02-14 15:16:24 +0100

    Implement AreaTrigger support for SmartAI

--- src/DAO/SmartAIDAO.php
+++ src/DAO/SmartAIDAO.php
@@ -5,63 +5,84 @@ namespace SUN\DAO;
 use SUN\Domain\Creature;
 use SUN\Domain\Gameobject;
 use SUN\Domain\Line;
-use SUN\Domain\Text;
 
-class SmartAIDAO extends DAO {
-	public function getAvailableEntry() {
+class SmartAIDAO extends DAO
+{
+	/**
+	 * Get the next available entry for the SmartAI Tutorial.
+	 *
+	 * @return mixed
+	 */
+	public function getAvailableEntry()
+	{
 		return $this->getDb('test')->fetchAssoc('SELECT entryorguid FROM smart_scripts WHERE entryorguid BETWEEN 50000 AND 50099 ORDER BY entryorguid DESC LIMIT 1');
 	}
 
-	/*
-	 * GET CREATURE ENTRY SCRIPT
+	/**
+	 * Get the Creature Entry SmartAI.
+	 *
+	 * @param Creature $creature
+	 * @return array
 	 */
-	public function getCreatureEntryScript(Creature $creature) {
+	public function getCreatureEntryScript(Creature $creature)
+	{
 		$all = $this->getDb('test')->fetchAll('SELECT * FROM smart_scripts WHERE entryorguid = ? AND source_type = 0', array($creature->getEntry()));
 		$lines = [];
-		foreach($all as $line) {
+		foreach($all as $line)
 			$lines[$line['id']] = new Line($line);
-		}
 		return $lines;
 	}
 
-	/*
-	 * GET CREATURE GUID SCRIPT
+	/**
+	 * Get the Creature Guid SmartAI.
+	 *
+	 * @param Creature $creature
+	 * @return array
 	 */
-	public function getCreatureGuidScript(Creature $creature) {
+	public function getCreatureGuidScript(Creature $creature)
+	{
 		$all = $this->getDb('test')->fetchAll('SELECT * FROM smart_scripts WHERE entryorguid = -? AND source_type = 0', array($creature->getGuid()));
 		$lines = [];
-		foreach($all as $line) {
+		foreach($all as $line)
 			$lines[$line['id']] = new Line($line);
-		}
 		return $lines;
 	}
 
-	/*
-	 * GET GAMEOBJECT ENTRY SCRIPT
+	/**
+	 * Get the GameObject Entry SmartAI.
+	 *
+	 * @param Gameobject $go
+	 * @return array
 	 */
-	public function getGOEntryScript(Gameobject $go) {
+	public function getGOEntryScript(Gameobject $go)
+	{
 		$all = $this->getDb('test')->fetchAll('SELECT * FROM smart_scripts WHERE entryorguid = ? AND source_type = 1', array($go->getEntry()));
 		$lines = [];
-		foreach($all as $line) {
+		foreach($all as $line)
 			$lines[$line['id']] = new Line($line);
-		}
 		return $lines;
 	}
 
-	/*
-	 * GET GAMEOBJECT GUID SCRIPT
+	/**
+	 * Get the GameObject Guid SmartAI.
+	 *
+	 * @param Gameobject $go
+	 * @return array
 	 */
-	public function getGOGuidScript(Gameobject $go) {
+	public function getGOGuidScript(Gameobject $go)
+	{
 		$all = $this->getDb('test')->fetchAll('SELECT * FROM smart_scripts WHERE entryorguid = -? AND source_type = 1', array($go->getGuid()));
 		$lines = [];
-		foreach($all as $line) {
+		foreach($all as $line)
 			$lines[$line['id']] = new Line($line);
-		}
 		return $lines;
 	}
 
-	/*
-	 * GET SCRIPT
+	/**
+	 * Get SmartAI Script.
+	 *
+	 * @param $script
+	 * @return array
 	 */
 	public function getScript($script) {
 		$all = $this->getDb('test')->fetchAll('SELECT * FROM smart_scripts WHERE entryorguid = ? AND source_type = 9', array($script));
@@ -72,42 +93,94 @@ class SmartAIDAO extends DAO {
 		return $lines;
 	}
 
-	public function getEvents() {
+	/**
+	 * Get AreaTrigger script.
+	 *
+	 * @param $entry
+	 * @return array
+	 */
+	public function getAreaTriggerScript($entry)
+	{
+		$all = $this->getDb('test')->fetchAll('SELECT * FROM smart_scripts WHERE entryorguid = ? AND source_type = 2', array(intval($entry)));
+		$lines = [];
+		foreach($all as $line) {
+			$lines[$line['id']] = new Line($line);
+		}
+		return $lines;
+	}
+
+	/**
+	 * Get all the SmartAI Events.
+	 *
+	 * @return array
+	 */
+	public function getEvents()
+	{
 		$fetch = $this->getDb('tools')->fetchAll('SELECT * FROM smartai_events');
 		$events = [];
-		foreach($fetch as $event) {
+		foreach($fetch as $event)
 			$events[$event['id']] = $event;
-		}
 		return $events;
 	}
 
-	public function getEvent($event) {
-		return $this->getDb('tools')->fetchAssoc('SELECT * FROM smartai_events WHERE id = ?', array($event));
+	/**
+	 * Get the #id SmartAI Event.
+	 *
+	 * @param $id
+	 * @return mixed
+	 */
+	public function getEvent($id)
+	{
+		return $this->getDb('tools')->fetchAssoc('SELECT * FROM smartai_events WHERE id = ?', array($id));
 	}
 
-	public function getActions() {
+	/**
+	 * Get all the SmartAI Actions.
+	 *
+	 * @return array
+	 */
+	public function getActions()
+	{
 		$fetch = $this->getDb('tools')->fetchAll('SELECT * FROM smartai_actions');
 		$actions = [];
-		foreach($fetch as $action) {
+		foreach($fetch as $action)
 			$actions[$action['id']] = $action;
-		}
 		return $actions;
 	}
 
-	public function getAction($action) {
-		return $this->getDb('tools')->fetchAssoc('SELECT * FROM smartai_actions WHERE id = ?', array($action));
+	/**
+	 * Get the #id SmartAI Action.
+	 *
+	 * @param $id
+	 * @return mixed
+	 */
+	public function getAction($id)
+	{
+		return $this->getDb('tools')->fetchAssoc('SELECT * FROM smartai_actions WHERE id = ?', array($id));
 	}
 
-	public function getTargets() {
+	/**
+	 * Get all the SmartAI Targets.
+	 *
+	 * @return array
+	 */
+	public function getTargets()
+	{
 		$fetch = $this->getDb('tools')->fetchAll('SELECT * FROM smartai_targets');
 		$targets = [];
-		foreach($fetch as $target) {
+		foreach($fetch as $target)
 			$targets[$target['id']] = $target;
-		}
 		return $targets;
 	}
 
-	public function getTarget($target) {
-		return $this->getDb('tools')->fetchAssoc('SELECT * FROM smartai_targets WHERE id = ?', array($target));
+	/**
+	 * Get the #id SmartAI Target.
+	 *
+	 * @param $id
+	 * @return mixed
+	 */
+	public function getTarget($id)
+	{
+		return $this->getDb('tools')->fetchAssoc('SELECT * FROM smartai_targets WHERE id = ?', array($id));
 	}
 } 
